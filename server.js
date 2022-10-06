d989671308bf4268242ba3d94741e0ab3288732f
millyexe <milenalobko18@outlook.com>
2022-10-05 20:22:40 -0400

    undeted hobbies

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ app.get("/contact", function(req, res){
 res.send("contact me at: milenalobko.com");
 });
 
-app.get("/info", function(req, res){
-res.send("hi, this is Milly, this is my try of learning express. i really badly want my translator work.")
+app.get("/about", function(req, res){
+res.send("hi, this is Milly, this is my try of learning express. i really badly want my translator to work.")
 
-})
+app.get("/hobbies", function(req, res){
+
+    res.send("<ul><li>Coding</ul></li><ul><li>Gaming</ul></li>");
+});
+
+});
 app.listen(3000, function(){
 console.log("Server started on port 3000");
 });
